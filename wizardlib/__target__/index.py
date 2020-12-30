4765ceda91c40dda733429ae8ebd6da6f2a3a151
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-12-30 15:04:32 -0600

    Add README.

--- wizardlib/__target__/index.py
+++ wizardlib/__target__/index.py
@@ -1,3 +1,5 @@
 from builtins import *
 
 add_text("Hello, world!")
+add_text("Wut")
+add_text("Cool")
---
+++ wizardlib/build.py
@@ -0,0 +1,4 @@
+#!/home/daniel/Public/cwhq/src/wizardlib-transcrypt/.venv/bin/python
+
+import os
+os.system("python -m transcrypt -b -m -n index.py")
